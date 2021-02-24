5abb0af330ab0d2a34a8897191c25f03d01a3612
Alan Ferreira <alanjjr83@gmail.com>
2021-02-24 19:35:45 -0300

    Ultimo COmmit Metade Aula 02

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,11 +1,13 @@
+import styles from '../styles/components/ExperienceBar.module.css'
+
 export function ExperienceBar() {
   return (
-    <header className= "experience-bar">
+    <header className= {styles.experienceBar}>
       <span>0 xp</span>
         <div>
           {/* Esse style inline,é pq vai mudar, no css são coisas fixas */}
           <div style={{ width: '50%'}}/>
-          <span className= "current-experience" style= {{ left: '50%'}}>300 xp</span>
+          <span className= {styles.currentExperience} style= {{ left: '50%'}}>300 xp</span>
         </div>
       <span>600 xp</span>
     </header>
---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/AlanFerreiraDev.png" alt="Alan Ferreira"/>
+      <div>
+        <strong>Alan Ferreira</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
